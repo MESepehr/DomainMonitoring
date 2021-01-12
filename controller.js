9e4d6aea4c11957f3c25d8de94fbfe8d661a8cba
MESepehr <ebrahimsepehr@gmail.com>
2021-01-12 13:00:59 +0330

    Color warnings done

--- controller.js
+++ controller.js
@@ -1,5 +1,12 @@
 var blessed = require('blessed');
+var ping = require('ping');
+//Config
+const timeOut = 10*1000 ;
+const bestTime = 500 ;
+const itemPerLine = 3 ;
 
+//Params
+var maxWidth = 100/itemPerLine ;
 //Screen configuration
     var screen = blessed.screen({
       smartCSR: true
@@ -12,25 +19,45 @@ var blessed = require('blessed');
       return process.exit(0);
     });
 
-var myDomains = ['google.com','yahoo.com','192.168.1.1'];
+var myDomains = ['google.com','yahoo.com','192.168.1.1','192.168.7.1'];
 var diagrams = [{name:'',box:blessed.box()}];
     diagrams=[];
 
 for(var i = 0 ; i <myDomains.length; i++)
 {
   diagrams.push({name:myDomains[i],box:blessed.box({
-    top: Math.floor(i/2),
-    left: i%2==0?0:'50%',
-    width: '50%',
+    top: Math.floor(i/itemPerLine),
+    left: ((i%itemPerLine)*maxWidth)+'%',
+    width: maxWidth+'%',
     height: 1,
     content: myDomains[i],
-    style: {
-      bg: '#22ff22',
-      fg: 'black',
+    style:{
+      bg: '#0f0',
+      fg: 'black'
     }
   })});
+
+  loopMyPing(diagrams[i]);
 }
 
+function loopMyPing(data=diagrams[0])
+{
+    var requestTime = new Date().getTime();
+    ping.promise.probe(data.name, {
+        timeout: timeOut/1000,
+    }).then(function (res) {
+        var respondTime = Math.min(bestTime,new Date().getTime()-requestTime) ;
+
+        data.box.style.bg = !res.alive?'#f00':'#'+Math.floor((res.avg/bestTime)*0xf).toString(16)+Math.floor(((bestTime-res.avg)/bestTime)*0xf).toString(16)+'0';
+        data.box.content = data.name+((data.name!=res.numeric_host)?('('+res.numeric_host+')>'):'>')+(res.alive?respondTime:' ! ');
+
+        screen.append(data.box);
+        screen.render();
+
+
+        setTimeout(loopMyPing,1000,data);
+    });
+}
 
 //Screen rendering
   render();
@@ -45,65 +72,3 @@ for(var i = 0 ; i <myDomains.length; i++)
     screen.render();
   }
 
-
-
-return;
-
-var blessed = require('blessed');
-
-// Create a screen object.
-var screen = blessed.screen({
-  smartCSR: true
-});
- 
-screen.title = 'my window title';
-
-var box = blessed.box({
-    top: 0,
-    left: 0,
-    width: '50%',
-    height: 1,
-    content: 'Hello {bold}world{/bold}!',
-    tags: true,
-    style: {
-      bg: '#ff0000'
-    }
-  });
-
-   
-  // Append our box to the screen.
-  screen.append(box);
-
-  // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-    return process.exit(0);
-  });
-   
-  // Render the screen.
-  screen.render();
-
-  setInterval(render,33);
-
-  var precent = 50 ;
-
-  
-
-return;
-
-console.clear();
-console.log("hi");
-return
-
-var ping = require('ping');
-
-var hosts = ['192.168.7.1', 'google.com', 'yahoo.com'];
-
-hosts.forEach(function (host) {
-    // WARNING: -i 2 argument may not work in other platform like window
-    ping.promise.probe(host, {
-        timeout: 10,
-        extra: ['-i', '2'],
-    }).then(function (res) {
-        console.log(res);
-    });
-});
